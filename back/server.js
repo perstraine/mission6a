3e4b7d49f82964ab03c701196240ab6ee5ac1351
Simon Li <li.simon021@gmail.com>
2022-09-16 01:44:48 +1200

    added docker-compose & dockerfile front/back

--- back/server.js
+++ back/server.js
@@ -4,37 +4,37 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
-const url = "mongodb://localhost:27017";
+const url = "mongodb://mongo_db:27017";
 const client = new MongoClient(url);
 const dbName = "fruity";
 
 function quickSort(list) {
-  if (list.length < 2) return list;
-  let pivot = list[0];
-  let left = [];
-  let right = [];
-  for (let i = 1, total = list.length; i < total; i++) {
-    if (list[i].name < pivot.name) left.push(list[i]);
-    else right.push(list[i]);
-  }
-  return [...quickSort(left), pivot, ...quickSort(right)];
+	if (list.length < 2) return list;
+	let pivot = list[0];
+	let left = [];
+	let right = [];
+	for (let i = 1, total = list.length; i < total; i++) {
+		if (list[i].name < pivot.name) left.push(list[i]);
+		else right.push(list[i]);
+	}
+	return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
 async function main() {
-  await client.connect();
-  console.log("Connected successfully to server");
-  const db = client.db(dbName);
-  const collection = db.collection("fruits");
-  const findResult = await collection.find({}).toArray();
-  console.log("Found documents =>", findResult);
-  return findResult;
+	await client.connect();
+	console.log("Connected successfully to server");
+	const db = client.db(dbName);
+	const collection = db.collection("fruits");
+	const findResult = await collection.find({}).toArray();
+	console.log("Found documents =>", findResult);
+	return findResult;
 }
 
 app.get("/find", async (req, res) => {
-  const result = await main();
-  const sorted = quickSort(result);
-  console.log("Sorted documents =>", sorted);
-  res.send(sorted);
+	const result = await main();
+	const sorted = quickSort(result);
+	console.log("Sorted documents =>", sorted);
+	res.send(sorted);
 });
 
 app.listen(4000);
