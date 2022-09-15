a4c153cb7d3fdae4677ce954b01bebc2396c7b4a
annalittler <77206754+annalittler@users.noreply.github.com>
2022-09-15 16:43:37 +1200

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./styles.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Select from "react-select";
@@ -12,13 +13,10 @@ import {
 function App() {
   const [dbItems, setDbItems] = useState(undefined);
   const [errMsg, setErrMsg] = useState(false);
-  const [sortedItems, setSortedItems] = useState();
   const [sortMethod, setSortMethod] = useState("");
-  const [result, setResult] = useState();
   const [toPrint, setToPrint] = useState();
 
   const getDb = async (e) => {
-    // e.preventDefault();
     const response = await axios.get("http://localhost:4000/find");
     if (response.data.length > 0) {
       const dataToSort = response.data;
@@ -50,10 +48,6 @@ function App() {
     sort(sortMethod);
   }, [sortMethod]);
 
-  useEffect(() => {
-    console.log(toPrint);
-  }, [toPrint]);
-
   const sort = async (method) => {
     if (method === "nameAsc") {
       const newObj = await sortNameAsc(dbItems);
@@ -77,73 +71,65 @@ function App() {
 
   return (
     <div className="App">
-      {/* <button onClick={getDb}>CLICK</button> */}
-      {dbItems === undefined && <h1>FETCHING DATA....</h1>}
-      {errMsg === true && <h3>Failed to retrieve data. Try again.</h3>}
-      <Select
-        options={selectOptions}
-        onChange={handleChange}
-        defaultValue={selectOptions[0]}
-      />
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+      <div className="inner-container">
+        <h1>Fruit for sale!</h1>
+        {/* <h3>Please see our stock list below.</h3> */}
+        {dbItems === undefined && <h2>FETCHING DATA....</h2>}
+        {errMsg === true && <h3>Failed to retrieve data. Try again.</h3>}
+        <div className="results">
+          {dbItems !== undefined && (
+            <>
+              <label>Sort by:</label>
+              <Select
+                options={selectOptions}
+                onChange={handleChange}
+                defaultValue={selectOptions[0]}
+              />
+            </>
+          )}
+          {toPrint !== undefined && (
+            <div className="table">
+              <table>
+                <tbody>
+                  <tr key={"header"}>
+                    {Object.keys(toPrint[0]).map((key) => (
+                      <th>{key}</th>
+                    ))}
+                  </tr>
+                  {toPrint.map((item) => (
+                    <tr key={item.id}>
+                      {Object.values(item).map((val) => (
+                        <td>{val}</td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
 
-      {toPrint !== undefined && (
-        <table>
-          <tbody>
-            <tr key={"header"}>
-              {Object.keys(toPrint[0]).map((key) => (
-                <th>{key}</th>
-              ))}
-            </tr>
-            {toPrint.map((item) => (
-              <tr key={item.id}>
-                {Object.values(item).map((val) => (
-                  <td>{val}</td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-
-      {dbItems !== undefined && toPrint === undefined && (
-        <table>
-          <tbody>
-            <tr key={"header"}>
-              {Object.keys(dbItems[0]).map((key) => (
-                <th>{key}</th>
-              ))}
-            </tr>
-            {dbItems.map((item) => (
-              <tr key={item.id}>
-                {Object.values(item).map((val) => (
-                  <td>{val}</td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+          {dbItems !== undefined && toPrint === undefined && (
+            <div className="table">
+              <table>
+                <tbody>
+                  <tr key={"header"}>
+                    {Object.keys(dbItems[0]).map((key) => (
+                      <th>{key}</th>
+                    ))}
+                  </tr>
+                  {dbItems.map((item) => (
+                    <tr key={item.id}>
+                      {Object.values(item).map((val) => (
+                        <td>{val}</td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
